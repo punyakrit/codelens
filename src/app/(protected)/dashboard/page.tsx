f26a005c45a5056bb2c4d92be970cbf0b0885215
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-09-01 22:39:25 +0530

    dashboard template

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,15 +1,50 @@
-"use client"
-import useProject from '@/hooks/use-project';
-import { useUser } from '@clerk/nextjs'
-import React from 'react'
+"use client";
+import useProject from "@/hooks/use-project";
+import { useUser } from "@clerk/nextjs";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 function Page() {
   const { project } = useProject();
   return (
     <div>
-      {JSON.stringify(project)}
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        <div className="bg-primary flex w-fit items-center rounded-md px-4 py-3">
+          <Github className="size-5 text-white" />
+          <div className="ml-2">
+            <p className="text-sm font-medium text-white">
+              This project is linked to {" "}
+              <Link
+                href={project?.repoUrl || ""}
+                target="_blank"
+                className="inline-flex items-center text-white/80 hover:underline"
+              >
+                {project?.repoUrl}
+                <ExternalLink className="ml-1 size-4 text-white/80" />
+              </Link>
+            </p>
+          </div>
+        </div>
+        <div className="h-4">
+
+        </div>
+        <div className="flex items-center gap-x-4">
+        buttons
+        </div>
+      </div>
+      <div className="mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+          Asking questions
+          meetings
+        </div>
+        
+      </div>
+      <div className="mt-8">
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
